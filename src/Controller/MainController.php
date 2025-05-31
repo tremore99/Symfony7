752f60c2c807a38c0b804b1fbade878be50ea0f2
István Bosnyák <istvanbosnyak@hotmail.com>
2025-05-31 14:30:35 +0200

    Using our first form

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Form\PostForm;
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class MainController extends AbstractController
 {
-    #[Route('/main', name: 'app_main')]
+    #[Route('/', name: 'app_main')]
     public function index(): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+        $model = [
+            'controller_name' => 'MainController'
+        ];
+        
+        return $this->render('main/index.html.twig', $model);
+    }
+
+    #[Route('/create-post', name: 'create-post')]
+    public function createPost(): Response
+    {
+        $post = new Post();
+        $form = $this->createForm(PostForm::class, $post);
+
+        $model = [
+            'form' => $form->createView()
+        ];
+
+        return $this->render('main/post.html.twig', $model);
     }
 }
--- src/Form/PostForm.php
+++ src/Form/PostForm.php
@@ -6,14 +6,34 @@ use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class PostForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content')
+            ->add('title', TextType::class, [
+                'label' => 'Enter title',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Title'
+                ]
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Enter content',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Content'
+                ]
+            ])
+            ->add('save', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary mt-4',
+                ]
+            ])
         ;
     }
 
